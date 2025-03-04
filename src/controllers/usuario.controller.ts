3a16272a734e46c060fc4e46212b83e352fbd885
CarlosGrogu <carlos.gf@live.com>
2025-03-03 18:39:34 -0600

    Update usuariosController y prismaSchema

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,110 +1,110 @@
 import {PrismaClient, Usuario} from '@prisma/client';
 import {Router, Request, Response as ExpressResponse} from 'express';
-import {handleError} from '.';
 import {validateJWT} from '../middlewares/auth.middleware';
-import {Controller, Response} from '.';
+import {Response} from '.';
 import {generateJWT, verifyJWT} from '../utils/jwt';
 import bcrypt from 'bcryptjs';
 
-class UsuarioController implements Controller {
-  path: string;
-  router: Router;
-  prisma: PrismaClient;
+const UsuarioController = () => {
+  const prisma = new PrismaClient();
+  const router = Router();
 
-  constructor() {
-    this.router = Router();
-    this.prisma = new PrismaClient();
-    this.initializeRoutes();
-  }
-
-  initializeRoutes() {
-    this.router.get('/usuarios', validateJWT, this.getUsuarios);
-    this.router.get('/usuario/:idUsuario', validateJWT, this.getUsuarioById);
-    this.router.post('/usuario', validateJWT, this.addUsuario);
-    this.router.put('/usuario/:idUsuario', validateJWT, this.updateUsuario);
-    this.router.delete('/usuario/:idUsuario', validateJWT, this.deleteUsuario);
-    this.router.post('/usuarios/login', this.login);
-    this.router.post('/usuarios/login/token', this.loginToken);
-  }
-
-  getUsuarios = async (_: Request, res: ExpressResponse) => {
+  const getUsuarios = async (_: Request, res: ExpressResponse) => {
     console.log('GET /usuarios');
     try {
-      const usuarios = await this.prisma.usuario.findMany();
+      const usuarios = await prisma.usuario.findMany();
       res.json(usuarios);
     } catch (error) {
-      handleError(res, error, 'Usuario');
+      handleError(res, error);
     }
   };
 
-  getUsuarioById = async (req: Request, res: ExpressResponse) => {
+  const getUsuarioById = async (req: Request, res: ExpressResponse) => {
     console.log('GET /usuario/:idUsuario');
     try {
       const idUsuario = Number(req.params.idUsuario) ?? 0;
-      const usuario = await this.prisma.usuario.findUnique({
+      const usuario = await prisma.usuario.findUnique({
         where: {
           idUsuario,
         },
       });
       res.json(usuario);
     } catch (error) {
-      handleError(res, error, 'usuario');
+      handleError(res, error);
     }
   };
 
-  addUsuario = async (req: Request, res: ExpressResponse) => {
+  const addUsuario = async (req: Request, res: ExpressResponse) => {
     console.log('POST /usuario');
     try {
       const body: Usuario = req.body;
-      const usuarioCreado = await this.prisma.usuario.create({
-        data: {
-          ...body,
+      const usuario = await prisma.usuario.findFirst({
+        where: {
+          nombre: body.nombre ?? '',
         },
       });
-      res.json(usuarioCreado);
+      console.log('usuario', usuario);
+      if (usuario) {
+        const response: Response = {
+          status: false,
+          message: 'El usuario ya ha sido registrado. Por favor, verifica tus datos.',
+        };
+        return res.status(404).json(response);
+      }
+      const usuarioAdded = await prisma.usuario.create({
+        data: {
+          ...body,
+          contrasena: bcrypt.hashSync(body.contrasena!, bcrypt.genSaltSync()),
+        },
+      });
+      res.json(usuarioAdded);
     } catch (error) {
-      handleError(res, error, 'usuario');
+      handleError(res, error);
     }
   };
 
-  updateUsuario = async (req: Request, res: ExpressResponse) => {
+  const updateUsuario = async (req: Request, res: ExpressResponse) => {
     console.log('PUT /usuario/:idUsuario');
     try {
-      const idUsuario = Number(req.params.idUsuario) ?? 0;
+      const idUsuario = Number(req.params.idUsuario);
       const body: Usuario = req.body;
-      const usuarioActualizado = await this.prisma.usuario.update({
+      const usuarioUpdated = await prisma.usuario.update({
         where: {
-          idUsuario,
+          idUsuario: idUsuario ?? 0,
         },
         data: {
-          ...body,
+          idUsuario: body.idUsuario,
+          nombre: body.nombre,
+          contrasena: body.contrasena && bcrypt.hashSync(body.contrasena!, bcrypt.genSaltSync()),
+          ultimoAcceso: body.ultimoAcceso,
+          token: body.token,
         },
       });
-      res.json(usuarioActualizado);
+      res.json(usuarioUpdated);
     } catch (error) {
-      handleError(res, error, 'Usuario');
+      handleError(res, error);
     }
   };
 
-  deleteUsuario = async (req: Request, res: ExpressResponse) => {
+  const deleteUsuario = async (req: Request, res: ExpressResponse) => {
     console.log('DELETE /usuario/:idUsuario');
     try {
       const idUsuario = Number(req.params.idUsuario) ?? 0;
-      const usuarioEliminado = await this.prisma.usuario.delete({
+      const usuarioEliminado = await prisma.usuario.delete({
         where: {
           idUsuario,
         },
       });
       res.json(usuarioEliminado);
     } catch (error) {
-      handleError(res, error, 'Usuario');
+      handleError(res, error);
     }
   };
 
-  login = async (req: Request, res: ExpressResponse) => {
+  const login = async (req: Request, res: ExpressResponse) => {
     try {
       const body: Usuario = req.body;
-      const usuario = await this.prisma.usuario.findFirst({
+      const usuario = await prisma.usuario.findFirst({
         where: {
           nombre: body.nombre ?? '',
         },
@@ -125,7 +125,7 @@ class UsuarioController implements Controller {
         return res.status(404).json(response);
       }
       const token = await generateJWT(usuario.nombre!);
-      await this.prisma.usuario.updateMany({
+      await prisma.usuario.updateMany({
         where: {
           nombre: usuario.nombre ?? '',
           contrasena: usuario.contrasena ?? '',
@@ -145,11 +145,11 @@ class UsuarioController implements Controller {
       };
       res.json(response);
     } catch (error) {
-      this.handleError(res, error);
+      handleError(res, error);
     }
   };
 
-  loginToken = async (req: Request, res: ExpressResponse) => {
+  const loginToken = async (req: Request, res: ExpressResponse) => {
     try {
       const body: Usuario = req.body;
       if (!verifyJWT(body.token!)) {
@@ -159,7 +159,7 @@ class UsuarioController implements Controller {
         };
         return res.status(403).json(response);
       }
-      const usuario = await this.prisma.usuario.findFirst({
+      const usuario = await prisma.usuario.findFirst({
         where: {
           nombre: body.nombre ?? '',
           token: body.token ?? '',
@@ -173,7 +173,7 @@ class UsuarioController implements Controller {
         return res.status(404).json(response);
       }
       const token = await generateJWT(usuario.nombre!);
-      await this.prisma.usuario.updateMany({
+      await prisma.usuario.updateMany({
         where: {
           nombre: usuario.nombre ?? '',
           contrasena: usuario.contrasena ?? '',
@@ -193,11 +193,19 @@ class UsuarioController implements Controller {
       };
       res.json(response);
     } catch (error) {
-      this.handleError(res, error);
+      handleError(res, error);
     }
   };
 
-  handleError(res: ExpressResponse, error: any) {
+  router.get('/usuarios', getUsuarios);
+  router.get('/usuario/:idUsuario', validateJWT, getUsuarioById);
+  router.post('/usuario', addUsuario);
+  router.put('/usuario/:idUsuario', validateJWT, updateUsuario);
+  router.delete('/usuario/:idUsuario', deleteUsuario);
+  router.post('/usuarios/login', login);
+  router.post('/usuarios/login/token', loginToken);
+
+  const handleError = (res: ExpressResponse, error: any) => {
     const response: Response = {
       status: false,
       message: 'Ocurrió un error en UsuariosController',
@@ -205,7 +213,11 @@ class UsuarioController implements Controller {
     };
     console.log(`${error}`);
     return res.status(500).json(response);
-  }
-}
+  };
+
+  return {
+    router,
+  };
+};
 
 export default UsuarioController;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import createServer from './api';
 import config from './config/global';
-import {ClienteController, ServicioController} from './controllers';
+import {
+  ClienteController,
+  PedidoController,
+  PedidoDetalleController,
+  ServicioController,
+  UsuarioController,
+} from './controllers';
 
-const server = createServer([ClienteController(), ServicioController()], config);
+const server = createServer(
+  [ClienteController(), PedidoController(), PedidoDetalleController(), ServicioController(), UsuarioController()],
+  config,
+);
 server.run();
